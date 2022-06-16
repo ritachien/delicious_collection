aee7536077e7f12800b3b500288c8a58449eddd7
Rita Chien <ritachien929@gmail.com>
2022-06-17 02:00:22 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 require('./config/mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = 3000
@@ -18,6 +23,11 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 
 // start and listen on the Express server
